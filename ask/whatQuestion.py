abe0fe5ed240b8ff6ad07f04d2fdcad8fe6eb6c6
Aneesha Sampath <aneesha.sampath@outlook.com>
2020-11-07 18:51:08 -0500

    Finish implementing when/where/what

--- ask/whatQuestion.py
+++ ask/whatQuestion.py
@@ -16,8 +16,8 @@ warnings.filterwarnings("ignore")
 
 # input: 'Did Alan Turing live in England?', {Alan Turing: Who, England: Where}
 # or 'Is Alan Turing nice?' {Alan Turing: Who}
-def generateQFromQ(question, entity_dict):
-    questions_list = []
+def generateWhenWhereFromQ(question, entity_dict):
+    #questions_list = []
     doc = nlp(question)
 
     sent = list(doc.sents)[0]
@@ -33,29 +33,65 @@ def generateQFromQ(question, entity_dict):
                 if((entity_dict[word]=='When' or entity_dict[word]=='Where') and word in clause.text):
                     stringtotakeout = clause.text
                     questionword = entity_dict[word]
-
-        #if 'NP' in clause._.labels: 
-        #    for word in entity_dict: 
-        #        if()
-                 
-
-    question = question.split(stringtotakeout) 
-    question = questionword + ' ' + "".join(question)
-
-
-    print(question)
-
-
                     
-
+                    # prioritize when/where questions over who and what
+                    question = question.split(stringtotakeout) 
+                    question = questionword + ' ' + "".join(question)
+                    return question 
+        
     
 
 
-def generateQFromSentence(sentence, entity_dict):
-    raise Exception('not implemented')
+                    
+def generateWhoFromSentence(sentence, entity_dict):
+    doc = nlp(sentence)
+    sent = list(doc.sents)[0]
+    constituents = list(sent._.constituents)
+    stringtotakeout = ''
+    questionword = '' 
+    for clause in constituents:
+        if 'NP' in clause._.labels:
+            for word in entity_dict: 
+                if(entity_dict[word] == 'Who' and word == clause.text): 
+                    stringtotakeout = clause.text
+                    questionword = entity_dict[word]  
+                    question = sentence.split(stringtotakeout) 
+                    question = questionword + ' ' + "".join(question)
+                    return question[:len(question)-1] + '?'
+    
+    return "" 
 
 
-generateQFromQ('Did Alan Turing live in England?', {'Alan Turing': 'Who', 'England': 'Where'})
-generateQFromQ('Was Alan Turing born in 1915?', {'Alan Turing': 'Who', '1915': 'When'})
-generateQFromQ('Is Carnegie Mellon in Pittsburgh, Pennsylvania?', {'Carnegie Mellon': 'What', 'Pittsburgh': 'Where', 'Pennsylvania': 'Where'})
-generateQFromQ('Is Alan Turing the father of computer science?', {'Alan Turing': 'Who'})
+def generateWhatFromQ(question): 
+    # Did Alan Turing like ice cream? --> What did Alan Turing like?
+    # deal with "what" questions - replace 
+    # direct object with "what"
+    prevtoken = None 
+    doc = nlp(question)
+    for token in doc:
+        if prevtoken is not None and 'compound' in prevtoken.dep_ and 'obj' in token.dep_:
+            stringtotakeout = prevtoken.text + ' ' + token.text        
+            questionword = 'What'
+            question = question.split(stringtotakeout) 
+            question = questionword + ' ' + "".join(question)
+            return question 
+        elif 'obj' in token.dep_:
+            stringtotakeout = token.text        
+            questionword = 'What'
+            question = question.split(stringtotakeout) 
+            question = questionword + ' ' + "".join(question)
+            return question    
+        
+
+        prevtoken = token
+        
+
+    return ""
+
+
+print(generateWhenWhereFromQ('Did Alan Turing live in England?', {'Alan Turing': 'Who', 'England': 'Where'}))
+print(generateWhenWhereFromQ('Was Alan Turing born in 1915?', {'Alan Turing': 'Who', '1915': 'When'}))
+print(generateWhenWhereFromQ('Is Carnegie Mellon in Pittsburgh, Pennsylvania?', {'Carnegie Mellon': 'What', 'Pittsburgh': 'Where', 'Pennsylvania': 'Where'}))
+print(generateWhoFromSentence('Alan Turing is the father of computer science?', {'Alan Turing': 'Who'}))
+print(generateWhoFromSentence('Alan Turing lived in England.', {'Alan Turing': 'Who', 'England': 'Where'}))
+print(generateWhatFromQ('Did Alan Turing like ice cream?'))
